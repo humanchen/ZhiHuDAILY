bbe35599aa5b9037a63834c016482c419264a0e5
humanchen <human@humandeMac.local>
2017-01-21 10:46:25 +0800

    引入rac

--- Podfile
+++ Podfile
@@ -3,4 +3,6 @@ use_frameworks!
 target 'ZhiHuDAILY' do
 pod 'MMDrawerController'
 pod 'MJRefresh'
+#pod 'ReactiveCocoa', '~> 4.0.0'
+pod 'ReactiveObjC', '~> 2.1.0'
 end
--- Pods/Target Support Files/Pods-ZhiHuDAILY/Pods-ZhiHuDAILY-frameworks.sh
+++ Pods/Target Support Files/Pods-ZhiHuDAILY/Pods-ZhiHuDAILY-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MMDrawerController/MMDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveObjC/ReactiveObjC.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MMDrawerController/MMDrawerController.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReactiveObjC/ReactiveObjC.framework"
 fi
